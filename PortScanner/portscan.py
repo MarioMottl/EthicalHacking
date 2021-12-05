4eabe85f48dc21a7d052085aca97ab2192e5cc30
MarioMottl <mariomottl17@gmail.com>
2021-12-05 14:23:18 +0100

    Added comments

--- PortScanner/portscan.py
+++ PortScanner/portscan.py
@@ -1,24 +1,29 @@
 import pyfiglet
 import sys
+import os
 import socket
 from datetime import datetime
 import progressbar
 from multiprocessing import Pool
 from multiprocessing import freeze_support
 
-bar = progressbar.ProgressBar(max_value=int(sys.argv[3]))
-
+#sets the max value for the progressbar
+bar = progressbar.ProgressBar(max_value=int(sys.argv[2]))
+last = 0
+print("test")
 '''Define function to run mutiple processors and pool the results together'''
 def run_multiprocessing(func, i, n_processors):
     with Pool(processes=n_processors) as pool:
         return pool.map(func, i)
 
 def portscanner(port):
-
+    #get the targets host
     target = socket.gethostbyname(sys.argv[1])
-
+    global last
     try:
-        bar.update(port)
+        if last < port:
+            last = port
+            bar.update(port)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
 
@@ -39,13 +44,15 @@ def portscanner(port):
             sys.exit()
 
 def main():
-    if len(sys.argv) == 4:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if len(sys.argv) == 3:
         ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
         print(ascii_banner)
-        num_max = int(sys.argv[3])
-        n_processors =4
+        num_max = int(sys.argv[2])
+        n_processors = 8
         x_ls = list(range(num_max))
         out = run_multiprocessing(portscanner, x_ls, n_processors)
+    print("\n Scan completed. \n")
 
 
 if __name__ == "__main__":
